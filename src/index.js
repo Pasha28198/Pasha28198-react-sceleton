08a4f3477516d44e0bc883dd8446f07957c7009b
unknown <pavlognidin@gmail.com>
2018-02-02 19:42:37 +0200

    set up redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
 import { BrowserRouter } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
-import routes from './routes';
 import reducers from './store/reducers/index';
 import App from 'components/App';
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-const store = createStoreWithMiddleware(reducers);
+
+//connect store with redux dev tools
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(reduxThunk),
+));
+
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
